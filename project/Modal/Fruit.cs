e0d5ecfb13aafc21e7670727fedc1ee1fd671566
Jaimy Monsuur <jaimymonsuur@gmail.com>
2022-03-15 19:01:13 +0100

    terug

--- project/Modal/Fruit.cs
+++ project/Modal/Fruit.cs
@@ -7,12 +7,6 @@ namespace Modal
 {
     public class Fruit
     {
-        public Fruit(int id,string name, string description)
-        {
-            this.id = id;
-            this.name = name;   
-            this.description = description;
-        }
         public int id;
         public string name; 
         public string description; 
