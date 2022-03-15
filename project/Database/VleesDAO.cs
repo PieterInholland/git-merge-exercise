6aedc73e1aec0a4ce35912ef09e9dfa0ea29bc67
Bastiaan <667382@student.inholland.nl>
2022-03-15 16:22:58 +0100

    Spelling correction

--- project/Database/VleesDAO.cs
+++ project/Database/VleesDAO.cs
@@ -12,7 +12,7 @@ namespace Database
         Vlees vlees1 = new Vlees(1, "Grilworst", "Een verse grilworst van de slager.");
         Vlees vlees2 = new Vlees(2, "Ham", "Ham, lekker.");
         Vlees vlees3 = new Vlees(3, "Kippenbout", "Vers geslacht.");
-        Vlees vlees4 = new Vlees(4, "Dubious Food", "It's too gross to even look at. A bizarre smell issues forth from this heap. Eating it won,t hurt you, though...probably."); // ;)
+        Vlees vlees4 = new Vlees(4, "Dubious Food", "It's too gross to even look at. A bizarre smell issues forth from this heap. Eating it won't hurt you, though...probably."); // ;)
         Vlees vlees5 = new Vlees(5, "Gehaktbal", "Vers gemaakt door de lokale slager.");
         Vlees vlees6 = new Vlees(6, "Auf Wieder Schnitzel", "Auf wiedersehen!");
         Vlees vlees7 = new Vlees(7, "Spaanse Worstjes", "Je krijgt het hier spaans benauwd van.");
