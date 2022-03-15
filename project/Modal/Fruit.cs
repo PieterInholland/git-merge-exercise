4647a258472b2a0a99cb3ce7dbddcef714a5858c
Jaimy Monsuur <jaimymonsuur@gmail.com>
2022-03-15 15:35:52 +0100

    Start

---
+++ project/Modal/Fruit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modal
+{
+    internal class Fruit
+    {
+        public int id;
+        public string name; 
+        public string description; 
+    }
+}
